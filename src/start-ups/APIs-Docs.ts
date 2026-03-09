c0e8a7fdc824b8c61bb558e126f7453724548aae
Ndevu12 <niyokwizerwajeanpaulelisa@email.com>
2026-03-09 09:49:52 +0000

    Remove old backend implementations

--- src/start-ups/APIs-Docs.ts
+++
@@ -1,7 +0,0 @@
-import { app } from "../app";
-import swaggerDocs from "../config/swaggerOptions";
-import swaggerUi from "swagger-ui-express";
-
-export const Documentation = (): void => {
-  app.use("/docs", swaggerUi.serve as any, swaggerUi.setup(swaggerDocs));
-};
